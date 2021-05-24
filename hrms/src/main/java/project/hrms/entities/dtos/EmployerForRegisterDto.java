88e7e04d5fe71510298ab54e43436d585ed3edd2
Rıdvan <ridvanabay@gmail.com>
2021-05-24 16:14:54 +0300

    added Dtos EmployerForRegisterDto, JobSeekerForRegisterDto, UserForLoginDto

---
+++ hrms/src/main/java/project/hrms/entities/dtos/EmployerForRegisterDto.java
@@ -0,0 +1,17 @@
+package project.hrms.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EmployerForRegisterDto {
+    private String companyName;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String webSiteAddress;
+}
---
+++ hrms/src/main/java/project/hrms/entities/dtos/JobSeekerForRegisterDto.java
@@ -0,0 +1,20 @@
+package project.hrms.entities.dtos;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class JobSeekerForRegisterDto {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String nationalIdentity;
+    private String password;
+    private Date yearOfBirth;
+}
