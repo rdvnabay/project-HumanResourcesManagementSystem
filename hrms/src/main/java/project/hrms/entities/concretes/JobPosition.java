39fa0016e241ad6d0f3cbb2e0982c2444573a558
Rıdvan <ridvanabay@gmail.com>
2021-05-29 14:00:15 +0300

    Added JobPosition Entity

--- hrms/src/main/java/project/hrms/entities/concretes/JobPosition.java
+++ hrms/src/main/java/project/hrms/entities/concretes/JobPosition.java
@@ -1,14 +1,12 @@
 package project.hrms.entities.concretes;
 
-import lombok.Data;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
