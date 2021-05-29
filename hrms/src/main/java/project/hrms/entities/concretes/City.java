1403f84b5479564890e9cf276d321066d1c2aa32
Rıdvan <ridvanabay@gmail.com>
2021-05-29 13:58:11 +0300

    Added City Entity

---
+++ hrms/src/main/java/project/hrms/entities/concretes/City.java
@@ -0,0 +1,25 @@
+package project.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "name_cities")
+public class City {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String Name;
+}
