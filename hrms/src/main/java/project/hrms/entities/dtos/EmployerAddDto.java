9f27f44a8d5a0afec3c152bbe9a7552bf000adc3
Rıdvan <ridvanabay@gmail.com>
2021-06-01 23:36:33 +0300

    fixed #20 Deleted Property id

--- hrms/src/main/java/project/hrms/entities/dtos/EmployerAddDto.java
+++ hrms/src/main/java/project/hrms/entities/dtos/EmployerAddDto.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class EmployerAddDto {
-    private int id;
     private String email;
     private String password;
     private String companyName;
