e27236940f892966e456fd46bbd911c6a346d1a4
Rıdvan <ridvanabay@gmail.com>
2021-06-01 23:09:31 +0300

    fixed #18 Add CoverLetterAddDto

--- hrms/src/main/java/project/hrms/api/CoverLettersController.java
+++ hrms/src/main/java/project/hrms/api/CoverLettersController.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import project.hrms.business.abstracts.CoverLetterService;
-import project.hrms.entities.concretes.CoverLetter;
 import project.hrms.entities.dtos.CoverLetterAddDto;
 
 @RestController
