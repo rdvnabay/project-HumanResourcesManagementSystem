17741b2ea9d8f135c6049baba09627490e4c58b9
Rıdvan <ridvanabay@gmail.com>
2021-06-11 15:20:54 +0300

    Added JobBoardDto and CityUpdateDto Updated JobBoardAddDto

--- hrms/src/main/java/project/hrms/entities/dtos/JobBoard/JobBoardDto.java
+++ hrms/src/main/java/project/hrms/entities/dtos/JobBoard/JobBoardDto.java
@@ -1,6 +1,7 @@
 package project.hrms.entities.dtos.jobboard;
 
 import java.util.Date;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,14 +9,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
-public class JobBoardAddDto {
-    private int cityId;
-    private int employerId;
-    private int jobPositionId;
+public class JobBoardDto {
+    private int id;
+    // private String cityName;
+    // private String employerName; 
     private Date applicationDeadLine;
     private String description;
     private double maxSalary;
     private double minSalary;
     private int openPositionQuantity;
+    private String jobPositionName;
 }
