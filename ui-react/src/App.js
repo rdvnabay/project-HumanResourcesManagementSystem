d62aa8805af44ad40f4dcaf35ad331ed39cbb01f
Rıdvan <ridvanabay@gmail.com>
2021-06-12 23:14:34 +0300

    fixed #31 Code Refactoring

--- ui-react/src/App.js
+++ ui-react/src/App.js
@@ -1,22 +1,10 @@
 import "./App.css";
 import Dashboard from "./layouts/Dashboard";
-import Container from '@material-ui/core/Container';
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import { Route, Switch } from "react-router";
-import PublicLayout from "./layouts/PublicLayout";
-
 
 function App() {
   return (
     <div className="App">
-     
-      <Switch>
-      <Route path="/" component={Dashboard} />
-        <Route path="/register" component={Register} />
-        <Route path="/login" component={Login} />
-        <Route path="/public-layout" component={PublicLayout} />
-      </Switch>
+      <Dashboard/>
     </div>
   );
 }
--- ui-react/src/pages/CityList.jsx
+++ ui-react/src/pages/CityList.jsx
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from "react";
 import CityService from "../services/cityService";
+import { NavLink } from "react-router-dom";
 
 function CityList() {
   const [cities, setCities] = useState([]);
@@ -8,23 +9,68 @@ function CityList() {
     cityService.getCities().then((result) => setCities(result.data.data));
   });
   return (
-    <div>
-      <table className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th>#</th>
-            <th>Şehirler</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cities.map((city) => (
-            <tr key={city.id}>
-              <th>{city.id}</th>
-              <th>{city.name}</th>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="flex flex-col">
+      <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    #
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Şehir
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Status
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    Role
+                  </th>
+                  <th scope="col" className="relative px-6 py-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {cities.map((city) => (
+                  <tr key={city.id}>
+                       <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{city.id}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{city.name}</td>
+                 
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                        Active
+                      </span>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{city.name}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <NavLink to="#" className="text-indigo-600 hover:text-indigo-900">
+                        Edit
+                      </NavLink>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
