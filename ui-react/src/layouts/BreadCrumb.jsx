e4e6ea690864477d4d90008331b9230d019b6528
Rıdvan <ridvanabay@gmail.com>
2021-06-12 23:06:02 +0300

    Added React Component Header, Main, BreadCrumb

---
+++ ui-react/src/layouts/BreadCrumb.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function BreadCrumb() {
+  return (
+    <div>
+      <nav className="bg-grey-light p-3 rounded font-sans w-full m-4">
+        <ol className="list-reset flex text-grey-dark">
+          <li>
+            <NavLink to="#" className="text-blue font-bold">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <span className="mx-2">/</span>
+          </li>
+          <li>
+            <NavLink to="#" className="text-blue font-bold">
+              Library
+            </NavLink>
+          </li>
+          <li>
+            <span className="mx-2">/</span>
+          </li>
+          <li>Data</li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
