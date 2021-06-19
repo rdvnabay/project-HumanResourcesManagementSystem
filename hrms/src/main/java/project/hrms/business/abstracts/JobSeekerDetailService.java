09c4686d844c6a6b28bad4e8346f083ac40b15e9
Rıdvan <ridvanabay@gmail.com>
2021-06-19 23:22:05 +0300

    Code Refactoring and Delete JobSeekerDetail

--- hrms/src/main/java/project/hrms/business/abstracts/JobSeekerDetailService.java
+++
@@ -1,8 +0,0 @@
-// package project.hrms.business.abstracts;
-
-// import project.hrms.core.utilities.results.Result;
-// import project.hrms.entities.concretes.JobSeekerDetail;
-
-// public interface JobSeekerDetailService {
-//     Result add(JobSeekerDetail jobSeekerDetail);
-// }
--- hrms/src/main/java/project/hrms/dataAccess/abstracts/JobSeekerDetailDao.java
+++
@@ -1,9 +0,0 @@
-// package project.hrms.dataAccess.abstracts;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// import project.hrms.entities.concretes.JobSeekerDetail;
-
-// public interface JobSeekerDetailDao extends JpaRepository<JobSeekerDetail,Integer> {
-    
-// }
