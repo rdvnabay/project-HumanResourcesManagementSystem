42e577c88c166e8497e5c572e12838dd97040b92
Rıdvan <ridvanabay@gmail.com>
2021-06-21 23:37:16 +0300

    Add component Dashhboard, Sidebar, Footer and Sidebar

--- ui-react/src/layouts/Dashboard.jsx
+++ ui-react/src/layouts/Dashboard.jsx
@@ -3,18 +3,22 @@ import { Route } from "react-router";
 import Navi from "./Navi";
 import CityList from "../pages/CityList";
 import JobSeekerForLogin from "../pages/auth/JobSeekerForLogin";
-import BreadCrumb from "../layouts/BreadCrumb";
 import EmployerForRegister from "../pages/auth/EmployerForRegister";
 import JobSeekerForRegister from "../pages/auth/JobSeekerForRegister";
 import JobBoard from "../pages/jobBoard/JobBoard";
 import EmployerForLogin from "../pages/auth/EmployerForLogin";
+import UserProfile from "../pages/jobSeekerProfile/UserProfile";
+import Footer from "./Footer";
+import ForgotPassword from "../pages/auth/ForgotPassword";
+import NotFound from "../pages/NotFound";
+import Demo from "../pages/Demo";
+import Demo2 from "../pages/Demo2";
 
 export default function Dashboard() {
   return (
     <div className="grid grid-cols-1">
       <div>
         <Navi />
-        <BreadCrumb />
       </div>
       <Route exact path="/" component={JobBoard} />
       <Route exact path="/cities" component={CityList} />
@@ -22,6 +26,12 @@ export default function Dashboard() {
       <Route path="/aday/giris" component={JobSeekerForLogin} />
       <Route exact path="/isveren/kayit-ol" component={EmployerForRegister} />
       <Route path="/isveren/giris" component={EmployerForLogin} />
+      <Route path="/profile" component={UserProfile} />
+      <Route path="/forgot" component={ForgotPassword} />
+      <Route path="/notfound" component={NotFound} />
+      <Route path="/demo" component={Demo2} />
+      <Footer />
     </div>
+      
   );
 }
--- ui-react/src/layouts/Footer.jsx
+++ ui-react/src/layouts/Footer.jsx
@@ -1,9 +1,249 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
-    return (
-        <div>
-            ddd
+  return (
+    <div className="pt-12">
+      <footer id="footer" className="relative z-50 dark:bg-gray-900 pt-4">
+        <div className=" border-t border-b border-gray-200 dark:border-gray-700 py-4">
+          <div className="mx-auto container px-4 xl:px-12 2xl:px-4">
+            <div className="lg:flex">
+              <div className="w-full lg:w-1/2 mb-16 lg:mb-0 flex">
+                <div className="w-full lg:w-1/2 px-6">
+                  <ul>
+                    <li>
+                      <NavLink
+                        to=""
+                        className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50"
+                      >
+                        Components
+                      </NavLink>
+                    </li>
+                    <li className="mt-4">
+                      <NavLink
+                        to=""
+                        className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50"
+                      >
+                        Templates
+                      </NavLink>
+                    </li>
+                    <li className="mt-4">
+                    
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          Pricing
+                        </NavLink>
+               
+                    </li>
+                    <li className="mt-4">
+                     
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          FAQ
+                        </NavLink>
+                      
+                    </li>
+                    <li className="mt-4">
+                      <NavLink
+                        to=""
+                    
+                        className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50"
+                      >
+                        Documentation
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full lg:w-1/2 px-6">
+                  <ul>
+                    <li>
+                      
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          Free components
+                        </NavLink>
+                    
+                    </li>
+                    <li className="mt-4">
+                     
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          Blog
+                        </NavLink>
+                   
+                    </li>
+                    <li className="mt-4">
+                     
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          Changelog
+                        </NavLink>
+                    
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="w-full lg:w-1/2 flex">
+                <div className="w-full lg:w-1/2 px-6">
+                  <ul>
+                    <li>
+                      <NavLink
+                        to=""
+                      
+                        className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50"
+                      >
+                        Privacy policy
+                      </NavLink>
+                    </li>
+                    <li className="mt-4">
+                     
+                        <NavLink
+                        to="" className="text-xs lg:text-sm leading-none hover:text-brand dark:hover:text-brand text-gray-800 dark:text-gray-50">
+                          Terms of service
+                        </NavLink>
+                     
+                    </li>
+                  </ul>
+                </div>
+                <div className="w-full lg:w-1/2 px-6 flex flex-col justify-between">
+                  <div className="flex items-center mb-6">
+                    <NavLink
+                        to="">
+                      <div className="text-gray-800 dark:text-gray-50 cursor-pointer hover:text-brand dark:hover:text-brand ">
+                        <svg
+                          className="footer-icon feather feather-github"
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                        </svg>
+                      </div>
+                    </NavLink>
+                    <NavLink
+                        to="">
+                      <div className="pl-4">
+                        <svg
+                          className="footer-icon feather feather-twitter text-gray-800 dark:text-gray-50 cursor-pointer hover:text-brand dark:hover:text-brand "
+                          xmlns="http://www.w3.org/2000/svg"
+                          width={24}
+                          height={24}
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+                        </svg>
+                      </div>
+                    </NavLink>
+                  </div>
+                  <div className="relative w-36">
+                  
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-device-laptop"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <line x1={3} y1={19} x2={21} y2={19} />
+                        <rect x={5} y={6} width={14} height={10} rx={1} />
+                      </svg>
+                    
+                  
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-brightness-up"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx={12} cy={12} r={3} />
+                        <line x1={12} y1={5} x2={12} y2={3} />
+                        <line x1={17} y1={7} x2="18.4" y2="5.6" />
+                        <line x1={19} y1={12} x2={21} y2={12} />
+                        <line x1={17} y1={17} x2="18.4" y2="18.4" />
+                        <line x1={12} y1={19} x2={12} y2={21} />
+                        <line x1={7} y1={17} x2="5.6" y2="18.4" />
+                        <line x1={6} y1={12} x2={4} y2={12} />
+                        <line x1={7} y1={7} x2="5.6" y2="5.6" />
+                      </svg>
+                 
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute inset-0 m-auto ml-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-moon"
+                        width={20}
+                        height={20}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z" />
+                      </svg>
+                  
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="pointer-events-none absolute inset-0 m-auto mr-3 text-gray-700 dark:text-gray-50 icon icon-tabler icon-tabler-chevron-down"
+                      width={16}
+                      height={16}
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      fill="none"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <polyline points="6 9 12 15 18 9" />
+                    </svg>
+                    <select
+                    
+                      className="w-full focus:outline-none pl-10 py-2 appearance-none flex items-center h-12 border rounded border-gray-700 dark:border-gray-50 text-sm leading-5 dark:bg-gray-900 dark:text-gray-50"
+                    >
+                      <option value="auto">Auto</option>
+                      <option value="light">Light</option>
+                      <option value="dark">Dark</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className="py-4 flex flex-col justify-center items-center">
+         
+           
+ 
+          <p className="mt-6 text-xs lg:text-sm leading-none text-gray-900 dark:text-gray-50">
+            2021 Tailwind UI Kit. All Rights Reserved.
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 }
